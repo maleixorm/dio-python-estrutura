4d0f7368f0c7c0f23876f90f1e8f0724bec42081
Marcos Aleixo <maknytz@gmail.com>
2025-10-31 13:25:07 -0300

    Declarando listas em python

---
+++ 01 - Listas/00_declarando_listas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maça", "uva"]
+print(frutas)
+
+nomes = []
+print(nomes)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 420000, 2020, 2900, "São Paulo", True]
+print(carro)
