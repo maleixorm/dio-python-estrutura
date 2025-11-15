b8386cc16907601b2637ad6d92895d55a910b1a4
Marcos Aleixo <maknytz@gmail.com>
2025-11-15 12:39:25 -0300

    Desafio sobre tuplas

---
+++ desafio.py
@@ -0,0 +1,10 @@
+def soma_tupla(tupla):
+    return sum(tupla)
+
+if __name__ == "__main__":
+    entrada = input()
+# No "TODO", abaixo: Defina tupla para receber os números inteiros:
+    elementos = tuple(map(int, entrada.split()))
+    
+    resultado = soma_tupla(elementos)
+    print(f"A soma dos elementos da tupla é: {resultado}")
