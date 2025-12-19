2a3ffd42b3ac7663e0c1b95634f1d7cef4e44aba
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-19 00:04:03 -0300

    Trabalhando com datas no Python

---
+++ 06 - Data e Hora/01_datetime.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime, time
+
+data = date(1988, 2, 21)
+print(data)
+
+print(date.today())
+
+data_hora = datetime(1988, 2, 21, 20, 50, 32)
+print(data_hora)
+
+print(datetime.today())
+
+hora = time(10, 20, 0)
+print(hora)
